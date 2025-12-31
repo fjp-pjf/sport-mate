c4f4a644dc121d9398932ac1bb7b8ccaeb3184cf
fjp-pjf <feminjustin07@gmail.com>
2025-12-31 20:13:47 +0530

    register page design complete

--- frontend/components/auth/RegisterForm.tsx
+++ frontend/components/auth/RegisterForm.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Image from "next/image";
 import { SubmitHandler, useForm } from "react-hook-form";
+import { FaFacebookSquare, FaGoogle } from "react-icons/fa";
 
 enum GenderEnum {
   female = "female",
@@ -24,33 +25,76 @@ const RegisterForm = () => {
   const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
 
   return (
-    <div className="flex justify-center items-center p-10 bg-green-950 m-auto">
-      <div className="flex flex-col gap-10">
-        <form className="flex flex-col gap-1" onSubmit={handleSubmit(onSubmit)}>
-          <input placeholder="First Name" {...register("firstName")} />
-          <input placeholder="Last Name" {...register("lastName")} />
-          <input placeholder="Email" {...register("email")} />
-          <input placeholder="Phone Number" {...register("phonenumber")} />
-          <input placeholder="DOB" {...register("dob")} />
-          <label>Gender Selection</label>
-          <select {...register("gender")}>
-            <option value="female">female</option>
-            <option value="male">male</option>
-            <option value="other">other</option>
-          </select>
-          <input placeholder="Password" {...register("password")} />
-          <input
-            placeholder="Password COnfirmation"
-            {...register("confirmPassword")}
-          />
-          <input type="submit" />
-        </form>
+    <div className="flex justify-center items-center">
+      <div className="flex justify-center items-center m-20 border-2 bg-[#e56b6f] border-[#e56b6f] rounded-md">
+        <div className="flex justify-center items-center flex-col p-8 gap-2 border-[#e56b6f] w-100 rounded-l">
+          <form
+            className="flex flex-col gap-1 items-center text-black justify-center"
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            <input
+              className="px-[6px] py-[2px] border-2 rounded-md text-black bg-white  border-[#e56b6f] w-60"
+              placeholder="First name"
+              {...register("firstName")}
+            />
+            <input
+              className="px-[6px] py-[2px] border-2 rounded-md border-[#e56b6f] bg-white text-black w-60"
+              placeholder="Last name"
+              {...register("lastName")}
+            />
+            <input
+              className="px-[6px] py-[2px] border-2 rounded-md border-[#e56b6f] bg-white text-black w-60"
+              placeholder="Email"
+              {...register("email")}
+            />
+            <input
+              className="px-[6px] py-[2px] border-2 rounded-md border-[#e56b6f] bg-white text-black w-60"
+              placeholder="Phone number"
+              {...register("phonenumber")}
+            />
+            <input
+              className="px-[6px] py-[2px] border-2 rounded-md border-[#e56b6f] bg-white text-black w-60"
+              placeholder="DOB"
+              {...register("dob")}
+            />
+            <select
+              className="px-[6px] py-[2px] border-2 rounded-md border-[#e56b6f] bg-white text-black w-60"
+              {...register("gender")}
+            >
+              <option value="female">Gender</option>
+              <option value="female">female</option>
+              <option value="male">male</option>
+              <option value="other">other</option>
+            </select>
+            <input
+              className="px-[6px] py-[2px] border-2 rounded-md border-[#e56b6f] bg-white text-black w-60"
+              placeholder="Password"
+              {...register("password")}
+            />
+            <input
+              className="px-[6px] py-[2px] border-2 rounded-md border-[#e56b6f] bg-white text-black w-60"
+              placeholder="Password Confirmation"
+              {...register("confirmPassword")}
+            />
+            <button className="font-bold bg-black p-2 text-[#e56b6f] rounded-md cursor-pointer w-60">
+              Register
+            </button>
+          </form>
 
-        <button>Login with Google</button>
-        <button>Login with Facebook</button>
-      </div>
-      <div>
-        <Image src="/soccer.svg" alt="sport" width={400} height={100} />
+          <p className="text-black font-black">Or</p>
+
+          <button className="flex justify-center items-center gap-2 font-medium bg-black p-2 text-[#e56b6f] rounded-md cursor-pointer w-60">
+            <FaGoogle />
+            Login with Google
+          </button>
+          <button className="flex justify-center items-center gap-2 font-medium bg-black p-2 text-[#e56b6f] rounded-md cursor-pointer w-60">
+            <FaFacebookSquare />
+            Login with Facebook
+          </button>
+        </div>
+        <div className="px-5">
+          <Image src="/sport-mate.svg" alt="sport" width={400} height={100} />
+        </div>
       </div>
     </div>
   );
